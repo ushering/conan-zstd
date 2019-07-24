67392e0a85aa4869c46bb804b01793fac2c2d7d7
Ruisheng Wang <rwang@wavecomp.com>
2019-07-24 15:28:24 -0700

    exports sources

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ from conans import ConanFile, CMake, tools
 
 class ZstdConan(ConanFile):
     name = "zstd"
-    version = "1.4.0"
+    version = "1.4.0_src"
     url = "https://github.com/bincrafters/conan-zstd"
     homepage = "https://github.com/facebook/zstd"
     description = "Zstandard - Fast real-time compression algorithm"
@@ -15,14 +15,14 @@ class ZstdConan(ConanFile):
     topics = ("conan", "zstd", "compression", "algorithm", "decoder")
     license = "BSD-3-Clause"
     exports = ["LICENSE.md"]
-    exports_sources = ['CMakeLists.txt']
     generators = 'cmake'
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
     _source_subfolder = "source_subfolder"
+    exports_sources = ['CMakeLists.txt', os.path.join(_source_subfolder, '*')]
 
-    def source(self):
+    def _source(self):
         sha256 = "63be339137d2b683c6d19a9e34f4fb684790e864fee13c7dd40e197a64c705c1"
         tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version), sha256=sha256)
         extracted_dir = self.name + "-" + self.version
